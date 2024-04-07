da245fea93d6edff0de8f8ed261fd1524ea7ca4a
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-04-08 01:12:55 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const limit = 15;
 
 export const getPhoto = async (value, page = 1) => {
-    const KEY = "9190280-87e8455cc30411d2efd850bc";
+    const KEY = "9190280-87e8455cc30411d2efd850bc0";
     const url = await axios.get(`https://pixabay.com/api/`, {
         params: {
             key: KEY,
